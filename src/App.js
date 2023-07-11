09ff8289f5471f09e966a57e725b62d609e59396
alishaiaz <shaiazali@gmail.com>
2023-07-11 18:42:06 +0600

    final update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import NavBar from "./components/NavBar";
 import "./css/App.css";
 import ContactPage from "./components/pages/Contact";
 import CoursePage from "./components/pages/Course";
-import { BrowserRouter, Routes, Route, redirect } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./components/pages/Home";
 import AboutPage from "./components/pages/About";
 import Footer from "./components/Footer";
@@ -28,10 +28,31 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/course" element={<CoursePage />} />
-          <Route path="*" element={<HomePage />} />
+          <Route path="/ConnectedLearning">
+            <Route
+              path="/ConnectedLearning/contact"
+              element={<ContactPage />}
+            />
+            <Route path="/ConnectedLearning/about" element={<AboutPage />} />
+            <Route path="/ConnectedLearning/course" element={<CoursePage />} />
+            <Route path="/ConnectedLearning/" element={<HomePage />} />
+          </Route>
+          <Route
+            path="/contact"
+            element={<Navigate to="/ConnectedLearning/contact" replace />}
+          />
+          <Route
+            path="/about"
+            element={<Navigate to="/ConnectedLearning/about" replace />}
+          />
+          <Route
+            path="/course"
+            element={<Navigate to="/ConnectedLearning/course" replace />}
+          />
+          <Route
+            path="*"
+            element={<Navigate to="/ConnectedLearning/" replace />}
+          />
         </Routes>
         <Footer />
       </BrowserRouter>
