91cc889dc7c873ea36a5a48f58c6b6995d13275b
alishaiaz <shaiazali@gmail.com>
2023-07-11 19:32:50 +0600

    update

--- src/App.js
+++ src/App.js
@@ -6,25 +6,11 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./components/pages/Home";
 import AboutPage from "./components/pages/About";
 import Footer from "./components/Footer";
+import imageFiles from "./files/_imageFiles";
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
       <BrowserRouter>
         <NavBar />
         <Routes>
