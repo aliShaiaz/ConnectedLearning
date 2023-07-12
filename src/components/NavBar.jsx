4076b616b9b1e2881bc919a06c494c20e78f6e14
alishaiaz <shaiazali@gmail.com>
2023-07-12 17:11:34 +0600

    Minor Bug Fixes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,51 +4,49 @@ import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <>
-      <div className="NavBar">
-        <Link to="/">
-          <img src={imageFiles.Logo} alt="Logo" />
-        </Link>
-        <ul>
-          <li>
-            <NavLink
-              className={({ isActive }) => (isActive ? "active" : "inactive")}
-              to="/ConnectedLearning/"
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive }) => (isActive ? "active" : "inactive")}
-              to="/ConnectedLearning/about"
-            >
-              About
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive }) => (isActive ? "active" : "inactive")}
-              to="/ConnectedLearning/course"
-            >
-              Course
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive }) => (isActive ? "active" : "inactive")}
-              to="/ConnectedLearning/contact"
-            >
-              Contact
-            </NavLink>
-          </li>
-        </ul>
-        <div>
-          <button onClick={() => alert("Logged In!")}>Login</button>
-          <button onClick={() => alert("Signed Up!")}>Sign-Up</button>
-        </div>
+    <div className="NavBar">
+      <Link to="/">
+        <img src={imageFiles.Logo} alt="Logo" />
+      </Link>
+      <ul>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+            to="/ConnectedLearning/"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+            to="/ConnectedLearning/about"
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+            to="/ConnectedLearning/course"
+          >
+            Course
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+            to="/ConnectedLearning/contact"
+          >
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+      <div>
+        <button onClick={() => alert("Logged In!")}>Login</button>
+        <button onClick={() => alert("Signed Up!")}>Sign-Up</button>
       </div>
-    </>
+    </div>
   );
 };
 
